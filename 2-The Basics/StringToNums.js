681c47cba273bef4cfeaa6b3b58d4504cd73f122
Daevah Taylaran <daevtayl@gmail.com>
2021-12-19 14:53:47 -0800

    just new additions

---
+++ 2-The Basics/StringToNums.js
@@ -0,0 +1,31 @@
+console.log(Number('1'));
+console.log(typeof Number('1'));
+
+//Number() constructor
+let exampleNaN = Number('foo');
+console.log(exampleNaN);
+
+//parseInt() function
+let exampleParse = parseInt('12');
+console.log(exampleParse);
+
+let num = '1200';
+num = parseInt(num);
+console.log(`num is a: ${typeof num}`);
+
+let fraction = parseInt('3.134');
+console.log(fraction);   // returns '3' number
+
+//parseInt limit is ~300 digits => outputs infinity
+let infin = parseInt('1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111')
+
+console.log(infin);
+
+//parseFloat() includes decimal 
+let dec = parseFloat('12.45');
+console.log(`dec is: ${dec}`);
+
+let decWithLetters = parseFloat('12.45hello');
+console.log(`dec is: ${decWithLetters}`);
+
+
--- 2-The Basics/operations.js
+++ 2-The Basics/operations.js
@@ -1,32 +1,34 @@
-//Add
+//Add (+)
 let firstNumAdd = 38;
 let secNumAdd = 4;
 let sum = firstNumAdd + secNumAdd;
 console.log(sum);
 
-//Subtraction
+//Subtraction (-)
 let minuend = 44;
 let subtrahend = 2;
 let diff = minuend - subtrahend;
 console.log(diff);
 
-//Multiplication
+//Multiplication (*)
 let firstMult = 7;
 let secMult = 6;
 let product = firstMult * secMult;
 console.log(product);
 
-//Division and Remainder
+//Division and Remainder (/)
 let dividend = 16;
 let divisor = 2.5;
 let quot = dividend / divisor;
 console.log(quot);
 
-//Remainder
+//Remainder Operator--Modulo (%)
 let dividendMod = 16;
 let divisorMod = 4;
 let modulo = dividendMod % divisorMod;
 console.log(modulo);
+
+
 
 
 
