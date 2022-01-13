9da298ec73c4d8dafcffd89388a5691731f8df97
Daevah Taylaran <daevtayl@gmail.com>
2022-01-12 22:28:35 -0800

    object

---
+++ 9-Objects/whatObjects.js
@@ -0,0 +1,10 @@
+let hunkle = [
+  {color: 'tan/brown'},
+  {food: 'mom\'s homecooked meal'},
+  {game: 'League of Legends'},
+  {style: 'comfortable'},
+  {ailment: 'night owl gamer'},
+  {'fave mood': `¯\\_(ツ)_//¯`},
+]
+
+console.log(hunkle);
