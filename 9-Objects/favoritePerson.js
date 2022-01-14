cc7405234a31043e61c3b2005e4b5a1814817d1a
Daevah Taylaran <daevtayl@gmail.com>
2022-01-13 17:22:30 -0800

    favoritePerson

---
+++ 9-Objects/favoritePerson.js
@@ -0,0 +1,81 @@
+// object created using object literal syntax
+let person = {
+  name: 'K.A.J.H',
+  birthdate: 'December 5',
+  astrology: { //LOL
+    sun: 'Sagittarius', 
+    moon: 'Cancer',
+    rising: 'Capricorn',
+    venus: 'Sagittarius',
+    mars: 'Libra',
+    mercury: 'Sagittarius',
+  },
+  hobbies: ['playing games', 
+  'working out',
+  'learning something new', 
+  'cooking', 
+  'baking', 
+  'fermenting', 
+  'coding',
+  'watching a show',
+  'watching youtube',
+  'trying to read',
+  'watching a documentary',
+  'woodworking',
+  'spending time with family',
+  'internet browsing', 
+  'bullying D.M.T',
+],
+  'favorite colors': [
+    'shades of blue',
+    'neutral tones',
+    'green',
+  ]
+}
+
+let hHobbies = person.hobbies;
+let hName = person.name
+let randInt = Math.floor(Math.random() * hHobbies.length);
+let hActivity = hHobbies[randInt];
+
+let moon = person.astrology.moon;
+
+console.log(`\nWhat is he doing right now?\n${hName} is ${hActivity} !\n`);
+
+let me = {
+  name: 'D.M.T',
+  birthdate: 'April 22',
+  astrology: { 
+    sun: 'Taurus', 
+    moon: 'Virgo',
+    rising: 'Gemini',
+    venus: 'Taurus',
+    mars: 'Aries',
+    mercury: 'Aries',
+  },
+  hobbies: ['coding', 
+  'reading mystery/thriller books',
+  'digital painting',
+  'trying out new cafés and restaurants',
+  'farmers\' market shopping',
+  'working out',
+  'boxing',
+  'cooking', 
+  'yoga',
+  'baking', 
+  'playing piano', 
+  'sewing',
+  'crocheting',
+  'researching fashion trends',
+  'watching comfort shows',
+  'spending time with friends',
+  'listening to podcasts',
+  'gardening',
+  'bullying K.A.J.H',
+],
+  'favorite colors': [
+    'lavender',
+    'light shades of green',
+    'warm tones',
+  ]
+}
--- 9-Objects/whatObjects.js
+++ 9-Objects/whatObjects.js
@@ -1,5 +1,7 @@
+const { colors } = require("prompt");
+
 let hunkle = [
-  {color: 'tan/brown'},
+  {colors: ['tan/brown', 'blue', 'green', 'earth tones', ['yellow', 'black', 'misc']]},
   {food: 'mom\'s homecooked meal'},
   {game: 'League of Legends'},
   {style: 'comfortable'},
@@ -7,4 +9,40 @@ let hunkle = [
   {'fave mood': `¯\\_(ツ)_//¯`},
 ]
 
-console.log(hunkle);
+let hunkleColors = hunkle[0];
+let colorValues = Object.values(hunkleColors);
+
+// different ways to call using values saved into variables
+let val = Object.values(hunkle[0]);
+let array = Object.values(hunkle[0])[0];
+let earthTones = Object.values(hunkle[0])[0][3];
+
+let colorsNestedArray = array[4];
+let misc = colorsNestedArray[2];
+
+
+
+// console.log(hunkle);
+// // console.log(hunkleColors);
+// console.log(colorValues);
+
+console.log(val); //outputs the arrays as a value [[ ]] , outer bracket indicate values, 1 value
+console.log(array); //outputs the first array/value for the key colors (the first element(an object)) in 'hunkle' object
+console.log(earthTones); //outputs the value/element at index 3 within the first array (array saved in variable 'array')
+
+console.log(colorsNestedArray);
+console.log(misc);
+
+
+console.log(hunkleColors); //outputs the object 'colors' with its properties (an array); hunkleColors is hunkle[0]; outputs { }
+console.log(hunkleColors[0]); //returns undefined because hunkleColors is an object--the first element in the 'hunkle', to get the value 'colors' within, you have to use Object.values
+console.log(colorValues); /*color values used Object.values on hunkleColors, (which is hunkle[0]--see above), the appropriate method to call for an object.
+colorValues outputs the VALUES of hunkleColors [[ ]] , outer bracket indicaes values, and inside [] shows there is one value
+*/
+console.log(colorValues[0]); /*outputs the actual array/first element value shown in colorValues
+specifies which VALUE you want to access from colorValues
+*/
+
+console.log(colorValues[0][0]);    //outputs the first element inside array *array is the property of 'colors'*
+console.log(colorValues[0][4]);    //outputs the last element--a nested array
+console.log(colorValues[0][4][0]); //outputs the first element of the nested array element
