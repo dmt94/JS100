3132ecaede53eae4d2f86ea6580717c54593b26d
D. Maries T <86685493+dmt94@users.noreply.github.com>
2022-01-28 21:21:18 -0800

    Delete 5-Functions directory

--- 5-Functions/add.js
+++
@@ -1,6 +0,0 @@
-function add(a, b) {
-  return a + b;
-}
-
-let twoAndThree = add(2, 3);
-console.log(twoAndThree);
--- 5-Functions/sayDefault.js
+++
@@ -1,7 +0,0 @@
-//structured to use a DEFAULT value 
-function say(words = "Hello") {
-  console.log(words + "!");
-}
-
-say("Howdy"); // => Howdy! with an argument
-say(); // => Hello! without an argument, uses default
