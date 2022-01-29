17a92ba6a93320f525503284bee65522e1784967
D. Maries T <86685493+dmt94@users.noreply.github.com>
2022-01-28 21:21:35 -0800

    Delete 8-Arrays directory

--- 8-Arrays/MyArray.js
+++
@@ -1,6 +0,0 @@
-const MyArray = [1, 2, 3];
-MyArray[1] = 5; //will update
-
-console.log(MyArray);
-MyArray = [4, 3, 6];
-console.log(MyArray); // Uncaught TypeError: Assignment to constant variable.
--- 8-Arrays/sort.js
+++
@@ -1,7 +0,0 @@
-let a = ["x", "c", "h", "b", "d", "e", "q"];
-let sortedA = a.sort();
-let revA = a.reverse();
-
-console.log(a);
-console.log(sortedA);
-console.log(revA);
