d6e21860cd9ef89eae6f4ddd9480ee7e6f2ac237
Daevah Taylaran <daevtayl@gmail.com>
2022-02-11 19:39:41 -0800

    some additions

--- JS100-Intro-Book/8-Arrays/slice.js
+++ JS100-Intro-Book/8-Arrays/slice.js
@@ -2,6 +2,8 @@
 slice method -- extracts and returns a PORTION of the array
 Two optional arguments are taken
 (extraction begins, extraction ends) end is not included
+
+(learn more)
 */
 
 let fruits = ['mango', 'orange', 'banana', 'pear', 'apple']
